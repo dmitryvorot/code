133beb5cd761db9f74d13b035eabdc93f6c2d25e
Dmytry <vorotnikov7009@mail.ru>
2023-03-01 02:29:30 +0300

    add function_title() to str_func.py in develop

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def function_upper(string):
     return string.upper()
+
+
+def function_title(string):
+    """Возращает строку, где каждое слово начинается с буквы в верхнем регистре"""
+    return string.title()
